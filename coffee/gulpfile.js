28be9aba6774fc494c1997c43625551af2b67eb7
sebastianOlivera <sebastian.osartori@correo.ucu.edu.uy>
2024-06-14 00:00:43 -0300

    seccion 23

--- coffee/gulpfile.js
+++ coffee/gulpfile.js
@@ -1,66 +1,63 @@
-import { src, dest, watch, series } from 'gulp';
-import gulpSass from 'gulp-sass';
-import * as sass from 'sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import imagemin from 'gulp-imagemin';
-import webp from 'gulp-webp';
-import avif from 'gulp-avif';
+const { src, dest, watch, series, parallel } = require('gulp');
 
-const sassObj = gulpSass(sass);
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
-// Manejo de errores
-function errorHandler(err) {
-    console.error(err.toString());
-    this.emit('end');
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )
+        .pipe( sass() )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
+        .pipe( dest('build/css') )
+
+    done();
 }
 
-// Tarea para compilar Sass
-function css() {
-    return src('src/scss/app.scss')
-        .pipe(sassObj().on('error', errorHandler))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(dest('build/css'));
-}
-
-// Tarea para optimizar imágenes
 function imagenes() {
     return src('src/img/**/*')
-        .pipe(imagemin({ optimizationLevel: 3 }))
-        .pipe(dest('build/img'));
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
 }
 
-// Tarea para generar versiones WebP de las imágenes
 function versionWebp() {
     const opciones = {
         quality: 50
-    };
+    }
     return src('src/img/**/*.{png,jpg}')
-        .pipe(webp(opciones))
-        .pipe(dest('build/img'));
+        .pipe( webp( opciones ) )
+        .pipe( dest('build/img') )
 }
-
-// Tarea para generar versiones AVIF de las imágenes
 function versionAvif() {
     const opciones = {
         quality: 50
-    };
+    }
     return src('src/img/**/*.{png,jpg}')
-        .pipe(avif(opciones))
-        .pipe(dest('build/img'));
+        .pipe( avif( opciones ) )
+        .pipe( dest('build/img'))
 }
 
-// Tarea para observar cambios durante el desarrollo
 function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', imagenes );
 }
 
-export const _css = css;
-export const _dev = dev;
-export const _imagenes = imagenes;
-export const _versionWebp = versionWebp;
-export const _versionAvif = versionAvif;
 
-// Tarea predeterminada que se ejecuta al llamar a "gulp" desde la línea de comandos
-export default series(imagenes, versionWebp, versionAvif, css, dev);
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
