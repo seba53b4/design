a207568f3ca7580e732b8b68dcc302dd0d6a39fd
seba53b4 <seba5364@hotmail.com>
2024-09-01 18:15:10 -0300

    featured property final

--- real-state/tailwind.config.mjs
+++ real-state/tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#3b4862",
+        secondary: "#6277a1",
+      },
       fontSize: {
         18: "18px",
         16: "16px",
@@ -17,6 +21,8 @@ export default {
       backgroundImage: {
         "header-gradient":
           "linear-gradient(90deg, rgba(59, 72, 98, .9) 0%, rgba(59, 72, 98, .9)), url('/img/header_bg.jpg')",
+        "featured-gradient":
+          "background-image: linear-gradient(90deg, transparent 0, transparent 50%, #3b4862 0, #3b4862), url(../img/destacada.jpg)",
       },
     },
   },
