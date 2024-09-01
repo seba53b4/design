75702d7eaa708d63df136bc51bcd9a884ad0e6d7
seba53b4 <seba5364@hotmail.com>
2024-09-01 14:53:27 -0300

    real-state: propiedades

---
+++ real-state/src/data/propiedades.js
@@ -0,0 +1,62 @@
+export const properties = [
+  {
+    title: "Casa de Lujo con 3 Cuartos",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "500,000",
+    imageSrc: "/img/propiedad_1.jpg",
+    imageAlt: "Casa de Lujo con 3 Cuartos",
+    href: "#",
+    className: "",
+  },
+  {
+    title: "Departamento Moderno en el Centro",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "350,000",
+    imageSrc: "/img/propiedad_2.jpg",
+    imageAlt: "Departamento Moderno en el Centro",
+    href: "#",
+    className: "",
+  },
+  {
+    title: "Casa Familiar con Jardín",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "600,000",
+    imageSrc: "/img/propiedad_3.jpg",
+    imageAlt: "Casa Familiar con Jardín",
+    href: "#",
+    className: "",
+  },
+  {
+    title: "Casa Familiar con Jardín",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "600,000",
+    imageSrc: "/img/propiedad_4.jpg",
+    imageAlt: "Casa Familiar con Jardín",
+    href: "#",
+    className: "",
+  },
+  {
+    title: "Casa Familiar con Jardín",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "600,000",
+    imageSrc: "/img/propiedad_5.jpg",
+    imageAlt: "Casa Familiar con Jardín",
+    href: "#",
+    className: "",
+  },
+  {
+    title: "Casa Familiar con Jardín",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ea pariatur dolores ducimus! Repellendus maiores nemo tempore, cupiditate pariatur expedita.",
+    price: "600,000",
+    imageSrc: "/img/propiedad_6.jpg",
+    imageAlt: "Casa Familiar con Jardín",
+    href: "#",
+    className: "",
+  },
+];
