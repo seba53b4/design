3a27a07f10ce47073ffd58d9ba998607391443fb
seba53b4 <seba5364@hotmail.com>
2024-09-02 23:55:45 -0300

    cardina-spa: header- swiper and welcome section

--- cardina-spa/tailwind.config.mjs
+++ cardina-spa/tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        custom: "1200px",
+      },
+      colors: {
+        primary: "#8ed3cc",
+        secondary: "#e082a8",
+        grey: "#979797",
+        light_grey: "#f8f8f8",
+      },
+      fontFamily: {
+        italianno: ["Italianno", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
